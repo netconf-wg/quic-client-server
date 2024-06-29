b005b38f836fd8d1e4475c15a7058942b511d6f9
Per Andersson <per.ietf@ionio.se>
2024-06-29 22:49:03 +0200

    Add client and server data module overview sections

--- refs/gen-trees.sh
+++ refs/gen-trees.sh
@@ -1,6 +1,8 @@
 echo "Generating tree diagrams..."
 
-pyang -f tree --tree-line-length 69 ../ietf-quic-client@*.yang \
-    > tree-ietf-quic-client.txt
-pyang -f tree --tree-line-length 69 ../ietf-quic-server@*.yang \
-    > tree-ietf-quic-server.txt
+pyang -f tree --tree-line-length 69 --tree-print-groupings \
+      --tree-no-expand-uses ../ietf-quic-client@*.yang \
+      > tree-ietf-quic-client.txt
+pyang -f tree --tree-line-length 69 --tree-print-groupings \
+      --tree-no-expand-uses ../ietf-quic-server@*.yang \
+      > tree-ietf-quic-server.txt
