9b1ad13048f4cceb5e665afa809214eb6ad7d80e
Per Andersson <per.ietf@ionio.se>
2024-10-19 12:42:03 +0200

    Add text for YANG module augments
    * Generate both -grouping and -augment YANG trees.

--- refs/gen-trees.sh
+++ refs/gen-trees.sh
@@ -1,8 +1,22 @@
 echo "Generating tree diagrams..."
 
+# ietf-quic-client
+# grouping
 pyang -f tree --tree-line-length 69 --tree-print-groupings \
       --tree-no-expand-uses ../ietf-quic-client@*.yang \
-      > tree-ietf-quic-client.txt
+      | grep -A 4 grouping > tree-ietf-quic-client-grouping.txt
+# augment
+pyang -f tree --tree-line-length 69 --tree-print-groupings \
+      --tree-no-expand-uses ../ietf-quic-client@*.yang \
+      | grep -A 4 augment > tree-ietf-quic-client-augment.txt
+
+# ietf-quic-server
+# grouping
 pyang -f tree --tree-line-length 69 --tree-print-groupings \
       --tree-no-expand-uses ../ietf-quic-server@*.yang \
-      > tree-ietf-quic-server.txt
+      | grep -A 4 grouping > tree-ietf-quic-server-grouping.txt
+# augment
+pyang -f tree --tree-line-length 69 --tree-print-groupings \
+      --tree-no-expand-uses ../ietf-quic-server@*.yang \
+      | grep -A 4 augment > tree-ietf-quic-server-augment.txt
+
